090be7459946e20e6235df45baaf926118d0b7e3
Bram Borggreve <beeman@beeman.nl>
2025-05-21 12:05:18 -0400

    feat: add base structure to docs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,7 +5,26 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    logging: {
+        fetches: {
+            fullUrl: true,
+        },
+    },
+    async redirects() {
+        return [
+            {
+                source: '/docs',
+                destination: '/docs/react/getting-started',
+                permanent: true,
+            },
+            {
+                source: '/docs/react',
+                destination: '/docs/react/getting-started',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default withMDX(config);

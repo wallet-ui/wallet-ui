450b4d0520290153f295c496d9143255cead9ccb
Bram Borggreve <beeman@beeman.nl>
2025-08-12 17:51:48 +0100

    feat: add Vercel Analytics

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './global.css';
+import { Analytics } from '@vercel/analytics/next';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
@@ -12,6 +13,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         <html lang="en" className={inter.className} suppressHydrationWarning>
             <body className="flex flex-col min-h-screen">
                 <RootProvider>{children}</RootProvider>
+                <Analytics />
             </body>
         </html>
     );

73732a8b85b9c19df16db500ef538223b085cbf1
Bram Borggreve <beeman@beeman.nl>
2025-08-12 14:00:56 +0100

    feat: add useWalletUiTransactionSignAndSend hook

---
+++ packages/react/src/use-wallet-ui-transaction-sign-and-send.tsx
@@ -0,0 +1,23 @@
+import type { IInstruction, TransactionSendingSigner } from 'gill';
+import { createTransaction, getBase58Decoder, signAndSendTransactionMessageWithSigners } from 'gill';
+
+import { useWalletUi } from './use-wallet-ui';
+
+export function useWalletUiTransactionSignAndSend() {
+    const { client } = useWalletUi();
+
+    return async (ix: IInstruction | IInstruction[], signer: TransactionSendingSigner) => {
+        const { value: latestBlockhash } = await client.rpc.getLatestBlockhash().send();
+
+        const transaction = createTransaction({
+            feePayer: signer,
+            instructions: Array.isArray(ix) ? ix : [ix],
+            latestBlockhash,
+            version: 0,
+        });
+
+        const signature = await signAndSendTransactionMessageWithSigners(transaction);
+
+        return getBase58Decoder().decode(signature);
+    };
+}

28aa562103f36748d7ad2ace6742e8466074b011
Kay ( Andrew ) <chauanhtuan185@gmail.com>
2025-12-16 21:19:59 +0700

    feat: add react example

--- examples/react-app/src/app/app-providers.tsx
+++ examples/react-app/src/app/app-providers.tsx
@@ -1,7 +1,17 @@
 import { PlaygroundProviders } from '@wallet-ui/playground-react';
 import { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router';
+import { registerLazorkitWallet } from '@lazorkit/wallet';
 
+registerLazorkitWallet(
+    {
+        rpcUrl: "https://api.devnet.solana.com",
+        paymasterConfig: {
+            paymasterUrl: "https://kor.devnet.solana.com",
+        }, 
+        portalUrl: "https://portal.lazor.sh"
+    }
+);
 export function AppProviders({ children }: { children: ReactNode }) {
     return (
         <BrowserRouter>

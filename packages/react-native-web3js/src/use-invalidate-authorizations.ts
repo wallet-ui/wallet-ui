c16c13e257858d7129b0efa50b6124c7d91216bf
beeman <beeman@beeman.nl>
2026-01-14 01:15:49 +0700

    refactor(react-native-web3js): clean up and isolate usage of authorization storage

--- packages/react-native-web3js/src/use-invalidate-authorizations.ts
+++
@@ -1,7 +0,0 @@
-import { useQueryConfig } from './use-query-config';
-
-export function useInvalidateAuthorizations() {
-    const { queryClient, queryKey } = useQueryConfig();
-
-    return () => queryClient.invalidateQueries({ queryKey });
-}
--- packages/react-native-web3js/src/use-query-config.ts
+++
@@ -1,9 +0,0 @@
-import { useQueryClient } from '@tanstack/react-query';
-
-export function useQueryConfig() {
-    const storageKey = 'authorization-cache';
-    const queryClient = useQueryClient();
-    const queryKey = ['wallet-authorization'];
-
-    return { queryClient, queryKey, storageKey };
-}
